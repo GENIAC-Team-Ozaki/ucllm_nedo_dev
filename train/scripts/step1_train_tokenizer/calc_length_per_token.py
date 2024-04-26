dad4c4ab472f794d9f2141f6962d499b78ef99f7
pass4 <fast.out1592@gmail.com>
2024-04-26 23:30:18 +0000

    トークン効率の悪い文章を確認する関数を追加

--- train/scripts/step1_train_tokenizer/calc_length_per_token.py
+++ train/scripts/step1_train_tokenizer/calc_length_per_token.py
@@ -25,11 +25,22 @@ def tokenize_text(sp, text_path, max_lines=None):
     tokens = sp.encode_as_pieces(text)
     return text, tokens
 
-def calculate_legnth_per_token(original_text, tokens):
+def calculate_length_per_token(original_text, tokens):
     original_length = len(original_text)
     tokenized_length = len(tokens)
-    legnth_per_token = original_length / tokenized_length
-    return legnth_per_token
+    length_per_token = original_length / tokenized_length
+    return length_per_token
+
+def inspect_tokenization(sp, text_path, max_lines=None):
+    with open(text_path, 'r', encoding='utf-8') as file:
+        for i, line in enumerate(file):
+            if max_lines is not None and i >= max_lines:
+                break
+            tokens = sp.encode_as_pieces(line)
+            length_per_token = calculate_length_per_token(line, tokens)
+            if length_per_token < 1.5 and len(line) > 5:
+                print(line)
+                print(tokens)
 
 def main():
     args = parse_arguments()
@@ -39,13 +50,17 @@ def main():
 
     sp = load_tokenizer(model_path)
 
+    #inspect_tokenization(sp, text_path, max_lines)
+
     original_text, tokens = tokenize_text(sp, text_path, max_lines)
 
-    legnth_per_token = calculate_legnth_per_token(original_text, tokens)
+    length_per_token = calculate_length_per_token(original_text, tokens)
 
     print(f'Original Length: {len(original_text)} characters')
     print(f'Tokenized Length: {len(tokens)} tokens')
-    print(f'legnth_per_token: {legnth_per_token:.2f}')
+    print(f'length_per_token: {length_per_token:.2f}')
+    #print(original_text)
+    #print(tokens)
     
 if __name__ == "__main__":
     main()
